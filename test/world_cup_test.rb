e19accf7f3fe7c48f8d4b1a61db67d113ad24c9b
tomaka <htomaka@treeptik.fr>
2018-12-16 18:16:16 +0100

    completed iteration 4

--- test/world_cup_test.rb
+++ test/world_cup_test.rb
@@ -26,7 +26,7 @@ class WorldCupTest < Minitest::Test
         france = Team.new("France")
         croatia = Team.new("Croatia") 
         world_cup = WorldCup.new(2018, [france, croatia])
-        
+
         assert_equal [france, croatia], world_cup.teams
     end
 
@@ -49,4 +49,28 @@ class WorldCupTest < Minitest::Test
         croatia.eliminated = true
         assert_equal [pogba], world_cup.active_players_by_position("midfielder")
     end
+
+    def test_get_all_players_by_position
+        france = Team.new("France")
+        mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+        pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
+        france.add_player(mbappe)
+        france.add_player(pogba)
+
+        croatia = Team.new("Croatia")
+        modric = Player.new({name: "Luka Modric", position: "midfielder"})
+        vida = Player.new({name: "Domagoj Vida", position: "defender"}) 
+        croatia.add_player(modric)
+        croatia.add_player(vida)
+
+        world_cup = WorldCup.new(2018, [france, croatia])
+
+        expected = {
+            "forward" => [mbappe],
+            "midfielder" => [pogba, modric],
+            "defender" => [vida]
+        }
+        assert_equal expected, world_cup.all_players_by_position
+    end
+
 end
